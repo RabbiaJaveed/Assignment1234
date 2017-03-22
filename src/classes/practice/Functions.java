0937d2184f2539f75e228c20360dd82e8930cba0
Fiaz <Fiaz@192.168.1.4>
2017-03-22 14:06:07 +0500

    its my program

---
+++ src/classes/practice/Functions.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.practice;
+
+/**
+ *
+ * @author Fiaz
+ */
+import java.util.Random;
+public class Functions {
+    
+
+public static void main (String []args)
+{
+Random as = new Random();
+int  n;
+n=as.nextInt(21)+20;
+System.out.println("RANDOM NUMBER IS="+n);
+System.out.println("RANDOM NUMBER sin IS="+Math.sin(n));
+System.out.println("RANDOM NUMBER tan IS="+Math.tan(n));
+System.out.println("RANDOM NUMBER IS="+Math.cos(n));
+}
+}
+
+    
+
