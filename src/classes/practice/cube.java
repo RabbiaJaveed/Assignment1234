a9716579ac88dd12529ce80ede1ef7c4534d9aa0
Fiaz <Fiaz@192.168.1.4>
2017-03-22 17:41:09 +0500

    its my program

---
+++ src/classes/practice/cube.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.practice;
+
+/**
+ *
+ * @author Fiaz
+ */
+import java.util.Scanner;
+
+public class cube {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n ,c;
+        System.out.println("Enter a number");
+        n=scan.nextInt();
+        c=n*n*n;
+        System.out.println("the cube is"+c);
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
